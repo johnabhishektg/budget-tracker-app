6054ac121e75204c36fb94262789cc18a502e3f3
john.tg <john.tg177@gmail.com>
2022-10-27 18:43:43 +0530

    adding the header of the app

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>My Budget Planner</h2>
+      <div className="header-disp">
+        <div className="disp-1">
+          <p>Budget:</p>
+          <button type="submit">edit</button>
+        </div>
+        <div className="disp-2">Remaning:</div>
+        <div className="disp-3">Spent so far:</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
